9ad6382cd1ef4fa78c4e3838614b5cda9ed6c429
Alejandra Ospina <aleospinagracia@hotmail.com>
2023-02-19 15:27:01 -0500

    correcciones finales

--- Tarea1/003_sistemadearchivos_00212243.c
+++ Tarea1/003_sistemadearchivos_00212243.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-
-void getPermission(unsigned int m)
+//Funcion para obtener los permisos de los archivos a partir del mode_t
+void printPermission(unsigned int m)
 {
 	putchar('\t');
 	putchar( m & S_IRUSR ? 'r' : '-' );
@@ -17,65 +17,31 @@ void getPermission(unsigned int m)
     putchar( m & S_IWOTH ? 'w' : '-' );
     putchar( m & S_IXOTH ? 'x' : '-' );
     putchar('\n');
+
 }
 
 int main(int argc, char **argv)
 {
 	//Varibles
-	char command[10] = "ls";
-	char commandInput[10];
-	struct dirent *dent;
-	DIR *directory;
-	char *path[50];
-	struct stat statistics;
-	char header[200];
+	char command[10] = "ls"; //Comando
+	char commandInput[10]; //Comando que se ingresa
+	struct dirent *dent; //
+	DIR *directory; //Guardar directorio
+	char *path[50]; //Guardar path
+	struct stat statistics; //Informacion de cada archivo
 
-	if(argc == 2) //Si se ingresan dos parametros 
-    {
-
-		//Copiar
-		strcpy(commandInput, argv[1]);
-
-		//Si es el primer parametro no es igual a cp no se hace la copia
-        if(strcmp(commandInput, command)!=0) 
-        {
-            printf("Comando incorrecto\n");
-            return 1;
-        }
-
-		//Se obtiene el path del directorio en el cual se est´a trabajando
-		getcwd(path, sizeof(path));
-
-		//Se abre el directorio
-		directory = opendir(path);
-
-		if(directory == NULL)
-		{
-			printf("No se pudo abrir el directorio %s\n", path);
-			return -1;
-		}
-
-		printf("Nombre\t\t\t\tTamaño(bytes)\tModo\n");
-		//Leer archivos del directorio
-		while((dent = readdir(directory)) != NULL)
-		{
-			stat(dent->d_name, &statistics);
-
-			printf("%-30s\t%d\t\t%d \n", dent->d_name, statistics.st_size, statistics.st_mode);
-		}
-
-		closedir(directory);
-		return 0;
-
-    }
-	else if(argc == 3) //Si se ingresan 3 parametros
+	
+	if(argc == 3 || argc == 2) //Si se ingresan 3 parametros
 	{
-
-		//Copiar
+		//Obtener comando y resto de parametros
 		strcpy(commandInput, argv[1]);
-		strcpy(path, argv[2]);
 
-		//Si es el primer parametro no es igual a cp no se hace la copia
+		if(argc == 2)
+			getcwd(path, sizeof(path));
+		if(argc == 3)
+			strcpy(path, argv[2]);
+
+		//Si es el primer parametro no es igual a ls 
         if(strcmp(commandInput, command)!=0) 
         {
             printf("Comando incorrecto\n");
@@ -85,21 +51,24 @@ int main(int argc, char **argv)
 		//Se abre el directorio
 		directory = opendir(path);
 
+		//Si el directorio actual o el proporcionado no se puede abrir
 		if(directory == NULL)
 		{
 			printf("No se pudo abrir el directorio %s\n", path);
 			return -1;
 		}
 
-		printf("\nName\t\t\t\tSize(bytes)\tMode\tPermissions\n");
 		//Leer archivos del directorio
+		printf("\nName\t\t\t\t\tSize(bytes)\tMode\tPermissions\n");
 		while((dent = readdir(directory)) != NULL)
 		{
 			stat(dent->d_name, &statistics);
-			printf("%-30s\t%-12d\t%-6X", dent->d_name, statistics.st_size, statistics.st_mode);
-			getPermission(statistics.st_mode);
+			printf("%-32s\t%-12d\t%-6X", dent->d_name, statistics.st_size, statistics.st_mode);
+			printPermission(statistics.st_mode);
 		}
 		printf("\n");
+
+		//Cerrar directorio
 		closedir(directory);
 		return 0;
 	}
