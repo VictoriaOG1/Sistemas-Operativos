69b37c619294ec6a6c32e158d0704ec72071aa11
Alejandra Ospina <aleospinagracia@hotmail.com>
2023-02-22 20:49:29 -0500

    Nuevo ejercicio

--- Ejercicios/threads_example.c
+++ Ejercicios/threads_example.c
@@ -6,27 +6,26 @@
 
 void *print_hello_world(void *tid)
 {
-    /* This function print the thread's identifier and then exits*/
+    /* This function prints the thread's identifier and then exits*/
     printf("Hello World. Greetings from thread %d\n", tid);
     pthread_exit(NULL);
 }
 
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-    /*The main program creates 10 threads and then exits Thread pool */
+    /*The main program creates 10 threads and then exits*/
     pthread_t threads[NUMBER_OF_THREADS];
     int status, i;
 
-    for(i=0; i<NUMBER_OF_THREADS; i++)
+    for(i=0; i< NUMBER_OF_THREADS; i++)
     {
         printf("Main here. Creating thread %d\n", i);
-        status = pthread_create(&threads[i], NULL, print_hello_world, (void*)i);
+        status = pthread_create(&threads[i], NULL, print_hello_world, (void *)i);
 
-        if(status != 0)
+        if(status !=0)
         {
             printf("Oops. pthread_create returned error code %d\n", status);
             exit(-1);
         }
     }
-    exit(NULL);
 }
